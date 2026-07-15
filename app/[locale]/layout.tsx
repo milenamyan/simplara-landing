55f848c268b05b147f8da3aeee9aa91056e66822
milenamyan <78661638+milenamyan@users.noreply.github.com>
2026-07-16 01:58:38 +0400

    up

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { getMessages, setRequestLocale } from 'next-intl/server';
 import { hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import { routing } from '@/i18n/routing';
 import ScrollToTop from '@/components/ScrollToTop';
 import "../globals.css";
@@ -73,6 +74,7 @@ export default async function LocaleLayout({
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
+        <Analytics />
       </body>
     </html>
   );
